02591308624c6df7b7f4bb5f19b971d7a1ebb6ac
JorbFreire <jorbfreire@ufrn.edu.br>
2020-10-14 11:07:26 -0300

    open and close restaurant

--- src/Components/MainMenu/RestaurantOpening/index.js
+++ src/Components/MainMenu/RestaurantOpening/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
 import { CheckCircleRounded } from '@material-ui/icons';
 
@@ -6,29 +6,44 @@ import api from '../../../services/api';
 import './styles.css';
 
 export default function RestaurantOpening() {
+  const [ restaurantIsOpen, setRestaurantIsOpen ] = useState(false);
+
   async function closeRestaurant() {
-    console.log('close');
     const response = await api.post('/close');
-    console.log(response);
+    setRestaurantIsOpen(response.data.open);
   }
 
   async function openRestaurant() {
-    console.log('open');
     const response = await api.post('/open');
-    console.log(response);
+    setRestaurantIsOpen(response.data.open);
   }
+
+  function controlRestaurantOpening() {
+    if(restaurantIsOpen)
+      closeRestaurant();
+    else
+      openRestaurant();
+    localStorage.setItem('restaurantIsOpen', restaurantIsOpen);
+  }
+
+  useEffect(() => {
+    const restaurantIsOpenOnStorage = localStorage.getItem('restaurantIsOpen');
+    setRestaurantIsOpen(restaurantIsOpenOnStorage);
+  }, []);
 
   return (
     <div className='restaurantOpening'>
-      <Button>
-        Fechar agora
+      <Button onClick={ controlRestaurantOpening } >
+        { restaurantIsOpen ? 'Fechar agora' : 'Abrir agora' }
       </Button>
 
       <div className='line' />
 
       <div className='info'>
         <div className='text'>
-          <h2>Restaurante aberto</h2>
+          <h2>
+            { restaurantIsOpen ? 'Restaurante aberto' : 'Restaurante fechado' }
+          </h2>
         </div>
 
         <CheckCircleRounded />
