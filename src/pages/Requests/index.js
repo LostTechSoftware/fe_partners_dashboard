9f3fe30d9337744fd0dd4a71f03131e51e0ba2e5
JorbFreire <jorbfreire@ufrn.edu.br>
2021-03-15 19:55:00 -0300

    remove random zero

--- src/pages/Requests/index.js
+++ src/pages/Requests/index.js
@@ -111,16 +111,16 @@ export default function Requests() {
       playFromPosition={1000}
       playStatus={Sound.status.Paused}
     />
-    {taskListNew.length
-     && <Sound 
-      url="https://serverem.s3.us-east-2.amazonaws.com/old_telephone.mp3" 
-      loop={true} 
-      autoLoad={true}
-      volume={100} 
-      playFromPosition={1000}
-      playStatus={Sound.status.PLAYING}
-      autoPlay={true}
-      />
+    {taskListNew.length ?
+      <Sound 
+        url="https://serverem.s3.us-east-2.amazonaws.com/old_telephone.mp3" 
+        loop={true} 
+        autoLoad={true}
+        volume={100} 
+        playFromPosition={1000}
+        playStatus={Sound.status.PLAYING}
+        autoPlay={true}
+      /> : null
     }
       <MainMenu currentPage='requests' />
       <div className='pageContent'>
