4a630bfab058cc6bc793443ba12561c796edc096
João Augusto <59159025+Joao208@users.noreply.github.com>
2021-07-10 17:53:57 -0300

    Update src/pages/Tasks/index.js

--- src/pages/Tasks/index.js
+++ src/pages/Tasks/index.js
@@ -279,7 +279,7 @@ export default function Tasks() {
                         </Additional>
                       ))}
                     </Collapsable>
-                  ) : null}
+                    ) : null}
                 </>
               ))}
             </>
