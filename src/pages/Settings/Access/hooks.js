ac7fc3a728f4065e80b3da6e9f2248d3c6c3328f
João Augusto <59159025+Joao208@users.noreply.github.com>
2021-07-11 12:42:28 -0300

    Update src/pages/Settings/Access/hooks.js

--- src/pages/Settings/Access/hooks.js
+++ src/pages/Settings/Access/hooks.js
@@ -1,6 +1,6 @@
 export const useAccess = () => {
   function getRandomColor() {
-    var letters = "0123456789ABCDEF";
+    let letters = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
